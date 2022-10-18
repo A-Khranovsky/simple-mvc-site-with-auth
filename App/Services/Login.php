a670b74a173cdf4c6f95dc7ff1eb7bfe64d3df94
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-10-18 18:56:15 +0300

    Fixed bugs in Login

--- App/Services/Login.php
+++ App/Services/Login.php
@@ -10,49 +10,49 @@ use App\models\User;
 class Login
 {
     //Checking if user is logged in
-    private $result;
+    private $user;
     public function __construct()
     {
-        //$user = new User;
-        if (isset($_SESSION['id'])) {
-//            if (isset($_COOKIE['login']) && isset($_COOKIE['password'])) {
-////                setcookie("login", "", time() - 1, '/');
-////                setcookie("password", "", time() - 1, '/');
-////                setcookie("login", $_COOKIE['login'], time() + 50000, '/');
-////                setcookie("password", $_COOKIE['password'], time() + 50000, '/');
-//                $this->result = true;
-//            } else {
-//                $user = $user->getAllById($_SESSION['id']);
-//                if ($user) {
-////                    setcookie("login", $user['login'], time() + 50000, '/');
-////                    setcookie("password", $user['password'], time() + 50000, '/');
-//                    $this->result = true;
-//                } else {
-//                    $this->result = false;
-//                }
-//            }
-            $this->result = true;
-        } else {
-//            if (isset($_COOKIE['login']) && isset($_COOKIE['password'])) {
-//                $user = $user->getAllByLogin($_COOKIE['login']);
-//                if ($user && $user['password'] == $_COOKIE['password']) {
-//                    $_SESSION['id'] = $user['id']; //записываем в сесиию id
-//                    $_SESSION['login'] = $user['login'];
-//                    $this->result = true;
-//                } else {
-//                    setcookie("login", "", time() - 360000, '/');
-//                    setcookie("password", "", time() - 360000, '/');
-//                    $this->result = false;
-//                }
-//            } else {
-//                $this->result = false;
-//            }
-            $this->result = false;
-        }
+        $this->user = new User;
     }
 
     public function __invoke()
     {
-        return $this->result;
+        if (isset($_SESSION['id'])) {
+            if (isset($_COOKIE['login']) && isset($_COOKIE['password'])) {
+                setcookie("login", "", time() - 1, '/');
+                setcookie("password", "", time() - 1, '/');
+                //exit(var_dump($_COOKIE['login']));
+                setcookie("login", $_COOKIE['login'], time() + 50000, '/');
+                setcookie("password", $_COOKIE['password'], time() + 50000, '/');
+                return true;
+            } else {
+                $user = $this->user->getAllById($_SESSION['id']);
+                if ($user) {
+                    setcookie("login", $user['login'], time() + 50000, '/');
+                    setcookie("password", $user['password'], time() + 50000, '/');
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            if (isset($_COOKIE['login']) && isset($_COOKIE['password'])) {
+                $user = $this->user->getAllByLogin($_COOKIE['login']);
+                if ($user && $user['password'] == $_COOKIE['password']) {
+                    $_SESSION['id'] = $user['id']; //записываем в сесиию id
+                    $_SESSION['login'] = $user['login'];
+                    return true;
+                } else {
+                    setcookie("login", "", time() - 360000, '/');
+                    setcookie("password", "", time() - 360000, '/');
+                    return false;
+                }
+            } else {
+                return false;
+            }
+            return false;
+        }
     }
 }
